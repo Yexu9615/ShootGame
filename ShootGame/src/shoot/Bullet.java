33404bada97286613e7a8c8f0b9084bcd3963fe6
alen <alen@192.168.1.100>
2018-10-30 19:01:57 +0100

    draw State

---
+++ ShootGame/src/shoot/Bullet.java
@@ -0,0 +1,23 @@
+package shoot;
+
+import java.util.Random;
+
+public class Bullet extends FlyingObject {
+	private int speed = 3;
+
+	public Bullet(int x, int y) {
+		image = ShootGame.bullet;
+		width = image.getWidth();
+		height = image.getHeight();
+		Random rand = new Random();
+		this.x = x;
+		this.y = y;
+	}
+
+	public void step() {
+		y-=speed;
+	}
+	public boolean outOfBounds() {
+		return y<-this.height;
+	}
+}
